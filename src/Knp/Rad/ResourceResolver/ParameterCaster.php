caf4a8d7f83dedf4d87f9b8e47138bd5faf28c5f
Einenlum <einenlum@vmail.me>
2015-01-29 17:18:22 +0100

    Fix

--- src/Knp/Rad/ResourceResolver/ParameterCaster.php
+++ src/Knp/Rad/ResourceResolver/ParameterCaster.php
@@ -4,6 +4,13 @@ namespace Knp\Rad\ResourceResolver;
 
 interface ParameterCaster
 {
+    /**
+     * @return bool
+     */
     public function supports($string);
+
+    /**
+     * @return mixed
+     */
     public function cast($string);
 }
--- src/Knp/Rad/ResourceResolver/Parser.php
+++ src/Knp/Rad/ResourceResolver/Parser.php
@@ -4,6 +4,13 @@ namespace Knp\Rad\ResourceResolver;
 
 interface Parser
 {
+    /**
+     * @return bool
+     */
     public function supports($string);
+
+    /**
+     * @return array
+     */
     public function parse($string);
 }
