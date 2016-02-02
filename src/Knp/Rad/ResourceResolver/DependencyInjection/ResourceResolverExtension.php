69bff3fd76fa4b6f37713959abad6793f0e3484a
David Jegat <david.jegat@knplabs.com>
2016-02-02 16:52:42 +0100

    Change services format to xml to avoid BC break with the sf3.* yaml parser

--- src/Knp/Rad/ResourceResolver/DependencyInjection/ResourceResolverExtension.php
+++ src/Knp/Rad/ResourceResolver/DependencyInjection/ResourceResolverExtension.php
@@ -5,7 +5,7 @@ namespace Knp\Rad\ResourceResolver\DependencyInjection;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
-use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
 
 class ResourceResolverExtension extends Extension
 {
@@ -14,7 +14,7 @@ class ResourceResolverExtension extends Extension
      */
     public function load(array $config, ContainerBuilder $container)
     {
-        $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.yml');
     }
 
