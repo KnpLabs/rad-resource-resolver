657138438e142dd865a21cc48e57b0f127e7bfeb
Carlos Adrian <Teshynil@users.noreply.github.com>
2020-02-11 20:35:23 -0600

    Upgrade to recent Symfony

--- src/Knp/Rad/ResourceResolver/Event/ResourceResolvedEvent/ResourceResolvedEvent.php
+++ src/Knp/Rad/ResourceResolver/Event/ResourceResolvedEvent/ResourceResolvedEvent.php
@@ -3,7 +3,7 @@
 namespace Knp\Rad\ResourceResolver\Event\ResourceResolvedEvent;
 
 use Knp\Rad\ResourceResolver\Event\ResourceResolvedEvent as EventInterface;
-use Symfony\Component\EventDispatcher\Event;
+use Symfony\Contracts\EventDispatcher\Event;
 
 final class ResourceResolvedEvent extends Event implements EventInterface
 {
